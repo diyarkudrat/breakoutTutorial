01156c4e68e6a45f84d9ae259afa7d16d91ef4a9
Diyar Kudrat <dkudrat@gmail.com>
2021-02-01 10:37:41 -0800

    moved event listeners to bottom of file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* CONSTANTS */
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
 
 const ballRadius = 10;
 
@@ -26,44 +28,15 @@ const LEFT = 'Left';
 const fontStyle = '16px Arial';
 
 /* VARIABLES */
-let x = canvas.width / 2;
-let y = canvas.height - 30;
+let x = canvasWidth / 2;
+let y = canvasHeight - 30;
 let dx = 2;
 let dy = -2;
 let score = 0;
 let lives = 3;
-let paddleX = (canvas.width - paddleWidth) / 2;
+let paddleX = (canvasWidth - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
-
-/* EVENT LISTENER FUNCTIONS */
-function mouseMoveHandler(e) {
-  const relativeX = e.clientX - canvas.offsetLeft;
-  if (relativeX > 0 && relativeX < canvas.width) {
-    paddleX = relativeX - paddleWidth / 2;
-  }
-}
-
-function keyDownHandler(e) {
-  if (e.key === RIGHT || e.key === ARROW_RIGHT) {
-    rightPressed = true;
-  } else if (e.key === LEFT || e.key === ARROW_LEFT) {
-    leftPressed = true;
-  }
-}
-
-function keyUpHandler(e) {
-  if (e.key === RIGHT || e.key === ARROW_RIGHT) {
-    rightPressed = false;
-  } else if (e.key === LEFT || e.key === ARROW_LEFT) {
-    leftPressed = false;
-  }
-}
-
-/* KEY/MOUSE EVENT LISTENERS */
-document.addEventListener('keydown', keyDownHandler, false);
-document.addEventListener('keyup', keyUpHandler, false);
-document.addEventListener('mousemove', mouseMoveHandler, false);
 
 /* GAME FUNCTIONS */
 function drawBricks() {
@@ -112,7 +85,7 @@ function drawScore() {
 function drawLives() {
   ctx.font = fontStyle;
   ctx.fillStyle = color;
-  ctx.fillText(`Lives: ${lives}`, canvas.width - 65, 20);
+  ctx.fillText(`Lives: ${lives}`, canvasWidth - 65, 20);
 }
 
 function drawBall() {
@@ -125,7 +98,7 @@ function drawBall() {
 
 function drawPaddle() {
   ctx.beginPath();
-  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.rect(paddleX, canvasHeight - paddleHeight, paddleWidth, paddleHeight);
   ctx.fillStyle = '#2824KG';
   ctx.fill();
   ctx.closePath();
@@ -145,11 +118,11 @@ function collisionPaddle() {
       alert('GAME OVER'); // eslint-disable-line no-alert
       document.location.reload();
     } else {
-      x = canvas.width / 2;
-      y = canvas.height - 30;
+      x = canvasWidth / 2;
+      y = canvasHeight - 30;
       dx = 3;
       dy = -3;
-      paddleX = (canvas.width - paddleWidth) / 2;
+      paddleX = (canvasWidth - paddleWidth) / 2;
     }
   }
 }
@@ -157,11 +130,11 @@ function collisionPaddle() {
 function collisionCanvas() {
   if (y + dy < ballRadius) {
     dy = -dy;
-  } else if (y + dy > canvas.height - ballRadius) {
+  } else if (y + dy > canvasHeight - ballRadius) {
     collisionPaddle();
   }
 
-  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+  if (x + dx > canvasWidth - ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
 }
@@ -169,8 +142,8 @@ function collisionCanvas() {
 function checkKeys() {
   if (rightPressed) {
     paddleX += 7;
-    if (paddleX + paddleWidth > canvas.width) {
-      paddleX = canvas.width - paddleWidth;
+    if (paddleX + paddleWidth > canvasWidth) {
+      paddleX = canvasWidth - paddleWidth;
     }
   } else if (leftPressed) {
     paddleX -= 7;
@@ -181,7 +154,7 @@ function checkKeys() {
 }
 
 function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
   drawBricks();
   drawBall();
@@ -198,6 +171,35 @@ function draw() {
 
   requestAnimationForm(draw); // eslint-disable-line no-undef
 }
+
+/* EVENT LISTENER FUNCTIONS */
+function mouseMoveHandler(e) {
+  const relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvasWidth) {
+    paddleX = relativeX - paddleWidth / 2;
+  }
+}
+
+function keyDownHandler(e) {
+  if (e.key === RIGHT || e.key === ARROW_RIGHT) {
+    rightPressed = true;
+  } else if (e.key === LEFT || e.key === ARROW_LEFT) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key === RIGHT || e.key === ARROW_RIGHT) {
+    rightPressed = false;
+  } else if (e.key === LEFT || e.key === ARROW_LEFT) {
+    leftPressed = false;
+  }
+}
+
+/* KEY/MOUSE EVENT LISTENERS */
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+document.addEventListener('mousemove', mouseMoveHandler, false);
 
 /* INITIALIZE GAME */
 draw();
